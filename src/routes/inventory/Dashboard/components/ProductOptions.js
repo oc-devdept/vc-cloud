157e33dc91c4acd17b87310cc90b35ad9ed56515
gjocdn <gianjie@ocdigitalnetwork.com>
2020-01-21 11:36:47 +0800

    master

--- src/routes/inventory/Dashboard/components/ProductOptions.js
+++ src/routes/inventory/Dashboard/components/ProductOptions.js
@@ -13,62 +13,44 @@ class Interior extends Component {
   }
  
   _RenderProductOptions = () => {
-
-    if(this.props.ProductOptions.length > 0) {
-
-      return (
-        <div style={{display: 'flex', flexDirection:'column'}}>
-          {this.props.ProductOptions.map((e, index) => {
-              
-            const key = Object.keys(e)[0]
-            const values = Object.values(e)[0]
-            
+   
+     return (
+        Object.keys(this.props.ProductOptions).map((ee, indexes) => {
+  
+          if(this.props.ProductOptions[ee].length > 0) {
             return (
-                  <div key={index} style={{margin :10, border: '1px black solid', flexDirection:'column'}} className="d-flex">
-                      
-                      <span>{key}</span>
-
-                      {values.length > 0 && 
-                        <div style={{display: 'flex', flexDirection:'row', overflow:'auto'}}>
-                          {values.map((each, indexes) =>{
-
-                            const item = each.productOption
-                            return (
-                              <div onClick={() => console.log(item)} key={indexes} style={{display: 'flex', flexDirection:'column', border : '1px solid black', borderStyle : 'dashed', margin: 10}}>
-                                  <span>name: {item.name}</span>
-                                  <span>price: {item.price}</span>
-                                  <span>{item.isDefault}</span>
-                                  {item.files.length > 0 && 
-                                      <Image
-                                        imageSource={item.files}
-                                        single={true}
-                                      />
-                                  }   
-                              </div>
-                            )
-                          })}
-                        </div>
-                      }
-
-                      {values.length == 0 && 
-                        <div>
-                          <span>No Product Option Found</span>
-                        </div>
-                      }
-
-                  </div>
-              )
-          })}
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          No Product Option Variant Found
-        </div>
-      )
-    }
+              <div key={indexes} style={{display:'flex', flexDirection:'row'}}>
     
+                <span>{ee}</span>
+    
+                {this.props.ProductOptions[ee].map((a, index) => {
+
+                    const item = a.productOption
+
+                    return (
+                      <div onClick={() => console.log(item)} key={indexes} style={{display: 'flex', flexDirection:'column', border : '1px solid black', borderStyle : 'dashed', margin: 10}}>
+                          <span>name: {item.name}</span>
+                          <span>price: {item.price}</span>
+                          <span>{item.isDefault}</span>
+                          {item.files.length > 0 && 
+                              <Image
+                                imageSource={item.files}
+                                single={true}
+                              />
+                          }   
+                      </div>
+                    )
+                })}
+              </div>
+            )
+
+          } else {
+            return null
+          }
+          
+        })
+      )
+
   }
 
 
