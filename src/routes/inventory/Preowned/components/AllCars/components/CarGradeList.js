06cd2509cfb5b4af7acf4d1ff26d7c39b426aa94
ianwwloc <ian@ocdigitalnetwork.com>
2020-10-30 09:23:01 +0800

    Updated add button

--- src/routes/inventory/Preowned/components/AllCars/components/CarGradeList.js
+++ src/routes/inventory/Preowned/components/AllCars/components/CarGradeList.js
@@ -7,13 +7,9 @@ import Image from "Components/Image";
 //import CarList from "Components/CarList";
 
 import { Edit, Delete } from "@material-ui/icons";
-import {
-  TableRow,
-  TableCell,
-  Button,
-  IconButton,
-  Switch
-} from "@material-ui/core";
+import { TableRow, TableCell, Button, IconButton, Switch } from "@material-ui/core";
+import { Icon } from "@iconify/react";
+import addFilled from "@iconify/icons-carbon/add-filled";
 import { withStyles } from "@material-ui/core/styles";
 import { orange } from "@material-ui/core/colors";
 
@@ -21,20 +17,20 @@ const PurpleSwitch = withStyles({
   switchBase: {
     color: orange[300],
     "&$checked": {
-      color: orange[500]
+      color: orange[500],
     },
     "&$checked + $track": {
-      backgroundColor: orange[500]
-    }
+      backgroundColor: orange[500],
+    },
   },
   checked: {},
-  track: {}
+  track: {},
 })(Switch);
 
 export default class Index extends PureComponent {
   state = {
     currentProduct: null,
-    ProductDetailLoading: false
+    ProductDetailLoading: false,
   };
 
   render() {
@@ -43,7 +39,7 @@ export default class Index extends PureComponent {
     const columns = [
       {
         name: "id",
-        options: { display: "excluded", filter: false, sort: false }
+        options: { display: "excluded", filter: false, sort: false },
       },
       {
         label: "NAME",
@@ -51,8 +47,8 @@ export default class Index extends PureComponent {
         options: {
           customBodyRender: (value, tableMeta) => {
             return value;
-          }
-        }
+          },
+        },
       },
       {
         label: "GRADE",
@@ -60,8 +56,8 @@ export default class Index extends PureComponent {
         options: {
           customBodyRender: (value, tableMeta) => {
             return value;
-          }
-        }
+          },
+        },
       },
       {
         label: "DESCRIPTION",
@@ -69,8 +65,8 @@ export default class Index extends PureComponent {
         options: {
           customBodyRender: (value, tableMeta) => {
             return value;
-          }
-        }
+          },
+        },
       },
       {
         label: "IMAGE",
@@ -78,14 +74,12 @@ export default class Index extends PureComponent {
         options: {
           customBodyRender: (value, tableMeta) => {
             if (value.length > 0) {
-              return (
-                <Image imageSource={value} single={true} thumbNail={true} />
-              );
+              return <Image imageSource={value} single={true} thumbNail={true} />;
             } else {
               return "No image";
             }
-          }
-        }
+          },
+        },
       },
       {
         label: "MILEAGE",
@@ -93,8 +87,8 @@ export default class Index extends PureComponent {
         options: {
           customBodyRender: (value, tableMeta) => {
             return value;
-          }
-        }
+          },
+        },
       },
       {
         label: "COST PRICE",
@@ -102,8 +96,8 @@ export default class Index extends PureComponent {
         options: {
           customBodyRender: (value, tableMeta) => {
             return value;
-          }
-        }
+          },
+        },
       },
       {
         label: "SELLING PRICE",
@@ -111,8 +105,8 @@ export default class Index extends PureComponent {
         options: {
           customBodyRender: (value, tableMeta) => {
             return value;
-          }
-        }
+          },
+        },
       },
       {
         name: "EDIT",
@@ -122,17 +116,12 @@ export default class Index extends PureComponent {
           empty: true,
           customBodyRender: (rowData, rowState) => {
             return (
-              <IconButton
-                onClick={() =>
-                  this.props.ToggleDialog("Selected_Grade", rowState.rowData[0])
-                }
-                size="small"
-              >
+              <IconButton onClick={() => this.props.ToggleDialog("Selected_Grade", rowState.rowData[0])} size="small">
                 <Edit style={{ fontSize: 14 }} />
               </IconButton>
             );
-          }
-        }
+          },
+        },
       },
       {
         name: "DELETE",
@@ -142,16 +131,13 @@ export default class Index extends PureComponent {
           empty: true,
           customBodyRender: (rowData, rowState) => {
             return (
-              <IconButton
-                onClick={() => this.props.DeleteCar(rowState.rowData[0])}
-                size="small"
-              >
+              <IconButton onClick={() => this.props.DeleteCar(rowState.rowData[0])} size="small">
                 <Delete style={{ fontSize: 14 }} />
               </IconButton>
             );
-          }
-        }
-      }
+          },
+        },
+      },
     ];
 
     const listOptions = {
@@ -166,16 +152,30 @@ export default class Index extends PureComponent {
       search: false,
       filter: false,
       setTableProps: () => ({ size: "small" }),
-      customToolbar: () => {
+      // customToolbar: () => {
+      //   return (
+      //     <Button
+      //       onClick={addGrade}
+      //       variant="contained"
+      //       color="primary"
+      //       size="small"
+      //     >
+      //       + Add Grade to {modelName}
+      //     </Button>
+      //   );
+      // },
+      customToolbar: (rowData, rowMeta) => {
+        // const data = {
+        //   MakeId: rowData[1],
+        //   ModelId: rowData[0],
+        //   make: rowData[3],
+        //   model: rowData[4],
+        // };
         return (
-          <Button
-            onClick={addGrade}
-            variant="contained"
-            color="primary"
-            size="small"
-          >
-            + Add Grade to {modelName}
-          </Button>
+          // <IconButton onClick={() => newDealFollowup()} size="small">
+          <IconButton onClick={() => this.props.ToggleDialog("Add_Grade", "", "")} size="small">
+            <Icon className="addIcon" icon={addFilled} width="2.5rem" height="2.5rem" color="#FF8B19" />
+          </IconButton>
         );
       },
       renderExpandableRow: (rowData, rowMeta) => {
@@ -186,18 +186,9 @@ export default class Index extends PureComponent {
             </TableCell>
           </TableRow>
         );
-      }
+      },
     };
 
-    return (
-      <RecordsList
-        title={title}
-        columns={columns}
-        data={tableData}
-        options={listOptions}
-        borderRadius={"0px"}
-        boxShadow={"none"}
-      />
-    );
+    return <RecordsList title={title} columns={columns} data={tableData} options={listOptions} borderRadius={"0px"} boxShadow={"none"} />;
   }
 }
