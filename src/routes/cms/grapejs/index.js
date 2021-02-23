93f2201686718e5110bca55447bce752fa57b166
edmund <edmund@ocdigitalnetwork.com>
2021-02-23 20:12:38 +0800

    WIP

--- src/routes/cms/grapejs/index.js
+++ src/routes/cms/grapejs/index.js
@@ -54,7 +54,78 @@ class GrapeJs extends Component {
                   }
                 ]
               },
+              layerManager: {
+                appendTo: '.layers-container'
+              },
+              // We define a default panel as a sidebar to contain layers
+              panels: {
+                defaults: [{
+                  id: 'layers',
+                  el: '.panel__right',
+                  // Make the panel resizable
+                  resizable: {
+                    maxDim: 350,
+                    minDim: 200,
+                    tc: 0, // Top handler
+                    cl: 1, // Left handler
+                    cr: 0, // Right handler
+                    bc: 0, // Bottom handler
+                    // Being a flex child we need to change `flex-basis` property
+                    // instead of the `width` (default)
+                    keyWidth: 'flex-basis',
+                  },
+                }]
+              }
           });
+
+          editor.Panels.addPanel({
+            id: 'panel-top',
+            el: '.panel__top',
+          });
+          editor.Panels.addPanel({
+            id: 'basic-actions',
+            el: '.panel__basic-actions',
+            buttons: [
+              {
+                id: 'visibility',
+                active: true, // active by default
+                className: 'btn-toggle-borders',
+                label: '<u>B</u>',
+                command: 'sw-visibility', // Built-in command
+              }, {
+                id: 'export',
+                className: 'btn-open-export',
+                label: 'Exp',
+                command: 'export-template',
+                context: 'export-template', // For grouping context of buttons from the same panel
+              }, {
+                id: 'show-json',
+                className: 'btn-show-json',
+                label: 'JSON',
+                context: 'show-json',
+                command(editor) {
+                  editor.Modal.setTitle('Components JSON')
+                    .setContent(`<textarea style="width:100%; height: 250px;">
+                      ${JSON.stringify(editor.getComponents())}
+                    </textarea>`)
+                    .open();
+                },
+              }
+            ],
+          });
+
+          var blockManager = editor.BlockManager;
+
+            // 'my-first-block' is the ID of the block
+            blockManager.add('my-first-block', {
+            label: 'Simple block',
+            content: '<div class="my-block">This is a simple block</div>',
+            });
+
+            editor.addComponents(`<div>
+  <img src="https://path/image" />
+  <span title="foo">Hello world!!!</span>
+</div>`);
     }
 
     render() {
@@ -64,11 +135,18 @@ class GrapeJs extends Component {
 
         return(
             <div>
-                <div id="gjs">
-                <h1>Hello World Component!</h1>
+                <div class="panel__top">
+                    <div class="panel__basic-actions"></div>
+                </div>
+                <div class="editor-row">
+                <div class="editor-canvas">
+                    <div id="gjs"><h1>Hello World Component!</h1></div>
+                </div>
+                <div class="panel__right">
+                    <div class="layers-container"></div>
+                </div>
                 </div>
                 <div id="blocks"></div>
-                <h1> HELLO</h1>
 
             </div>
         )
